06d0d217c0fd9e95694273d1c5a12d800ebbb1dc
mgclay <mary.clay@temple.edu>
2023-11-20 22:10:18 -0500

    change title and favicon

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//          HELLO!!!!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import About from './components/About.js'
 
@@ -34,7 +8,6 @@ const App = () => {
       <Route path = "/portfolio" element={<About/>}/>
     </Routes>
     </BrowserRouter>
-    // <div className='App'> hHELLO!!!!!!!!!</div>
   );
   };
 export default App
